087f852453efbfbfa452af15dfc59060a698723c
PomPerWoW <chatthanes22@gmail.com>
2024-11-11 02:04:43 +0700

    modify prolog algo in delaying

--- prolog/prolog_program.pl
+++ prolog/prolog_program.pl
@@ -4,60 +4,50 @@
 % Dynamic Declarations
 % =========================
 
-:- dynamic edge/3.
-:- dynamic mode_speed/2.
+:- dynamic edge/4.
+:- dynamic mode/2.
 
 % =========================
 % Map Representation
 % =========================
 
-% edge(Node1, Node2, Distance).
-% Define the edges of the graph with distances between nodes.
+% edge(Node1, Node2, Distance, Delay).
+% Define the edges of the graph with distances between nodes and delays.
 % Edges are defined in both directions to represent an undirected graph.
+% Delay is initialized to 0 for all edges.
 
-edge(a, b, 5).
-edge(b, a, 5).
+reset_edges :-
+    retractall(edge(_, _, _, _)),
+    % Re-define the edges in both directions with Delay = 0
+    assert(edge(a, b, 5, 0)), assert(edge(b, a, 5, 0)),
+    assert(edge(a, c, 10, 0)), assert(edge(c, a, 10, 0)),
+    assert(edge(b, c, 2, 0)), assert(edge(c, b, 2, 0)),
+    assert(edge(b, d, 3, 0)), assert(edge(d, b, 3, 0)),
+    assert(edge(c, d, 1, 0)), assert(edge(d, c, 1, 0)),
+    assert(edge(c, e, 7, 0)), assert(edge(e, c, 7, 0)),
+    assert(edge(d, e, 2, 0)), assert(edge(e, d, 2, 0)),
+    assert(edge(d, f, 3, 0)), assert(edge(f, d, 3, 0)),
+    assert(edge(e, f, 1, 0)), assert(edge(f, e, 1, 0)).
 
-edge(a, c, 10).
-edge(c, a, 10).
-
-edge(b, c, 2).
-edge(c, b, 2).
-
-edge(b, d, 3).
-edge(d, b, 3).
-
-edge(c, d, 1).
-edge(d, c, 1).
-
-edge(c, e, 7).
-edge(e, c, 7).
-
-edge(d, e, 2).
-edge(e, d, 2).
-
-edge(d, f, 3).
-edge(f, d, 3).
-
-edge(e, f, 1).
-edge(f, e, 1).
+% Initialize edges
+:- reset_edges.
 
 % =========================
 % Transportation Modes
 % =========================
 
-% mode_speed(Mode, SpeedInKmPerHour).
+% mode(Mode, SpeedInKmPerHour).
 % Define the speed for each mode of transportation.
 
-reset_speeds :-
-    retractall(mode_speed(_, _)),
-    assert(mode_speed(car, 60)),
-    assert(mode_speed(walking, 5)),
-    assert(mode_speed(motorcycle, 80)),
-    assert(mode_speed(airplane, 800)).
+reset_modes :-
+    retractall(mode(_, _)),
+    assert(mode(car, 60)),        % Car: 60 km/h
+    assert(mode(walking, 5)),     % Walking: 5 km/h
+    assert(mode(motorcycle, 80)), % Motorcycle: 80 km/h
+    assert(mode(airplane, 800)).  % Airplane: 800 km/h
 
-% Initialize mode speeds
-:- reset_speeds.
+% Initialize modes
+:- reset_modes.
 
 % =========================
 % Travel Time Calculation
@@ -67,11 +57,11 @@ reset_speeds :-
 % Calculate travel time based on distance and mode of transportation.
 
 travel_time(Distance, Mode, Time) :-
-    mode_speed(Mode, Speed),
+    mode(Mode, Speed),
     Time is Distance / Speed.
 
 % =========================
-% Dijkstras Algorithm
+% Dijkstra's Algorithm
 % =========================
 
 % Comparator predicate for predsort/3
@@ -86,15 +76,16 @@ shortest_path(Start, Goal, Path, Distance) :-
     dijkstra([[Start, [Start], 0]], Goal, RevPath, Distance),
     reverse(RevPath, Path).
 
-% Dijkstras helper predicate using predsort/3
+% Dijkstra's helper predicate using predsort/3
 dijkstra([[Goal, Path, Dist] | _], Goal, Path, Dist).
 dijkstra([[CurrentNode, CurrentPath, CurrentDist] | Rest], Goal, Path, Dist) :-
     findall(
         [NextNode, [NextNode | CurrentPath], NewDist],
         (
-            edge(CurrentNode, NextNode, D),
+            edge(CurrentNode, NextNode, D, Delay),
             \+ member(NextNode, CurrentPath),
-            NewDist is CurrentDist + D
+            TotalEdgeDistance is D + Delay,
+            NewDist is CurrentDist + TotalEdgeDistance
         ),
         NextNodes
     ),
@@ -111,9 +102,43 @@ dijkstra([[CurrentNode, CurrentPath, CurrentDist] | Rest], Goal, Path, Dist) :-
 % Succeeds if the edge exists, fails otherwise.
 
 remove_edge(Node1, Node2) :-
-    (   retract(edge(Node1, Node2, _)),
-        retract(edge(Node2, Node1, _))
+    (   retract(edge(Node1, Node2, _, _)),
+        retract(edge(Node2, Node1, _, _))
     ->  format('Obstacle introduced between ~w and ~w~n', [Node1, Node2])
+    ;   format('No edge exists between the specified nodes.~n'),
+        fail
+    ).
+
+% =========================
+% Delay Handling
+% =========================
+
+% add_delay(Node1, Node2, AdditionalDelay)
+% Adds a delay to the edge between Node1 and Node2.
+% Succeeds if the edge exists, fails otherwise.
+
+add_delay(Node1, Node2, AdditionalDelay) :-
+    (   retract(edge(Node1, Node2, Distance, CurrentDelay)),
+        NewDelay is CurrentDelay + AdditionalDelay,
+        assert(edge(Node1, Node2, Distance, NewDelay)),
+        retract(edge(Node2, Node1, Distance, _)),
+        assert(edge(Node2, Node1, Distance, NewDelay)),
+        format('Delay of ~w added between ~w and ~w~n', [AdditionalDelay, Node1, Node2])
+    ->  true
+    ;   format('No edge exists between the specified nodes.~n'),
+        fail
+    ).
+
+% remove_delay(Node1, Node2)
+% Removes any delay on the edge between Node1 and Node2.
+
+remove_delay(Node1, Node2) :-
+    (   retract(edge(Node1, Node2, Distance, _)),
+        assert(edge(Node1, Node2, Distance, 0)),
+        retract(edge(Node2, Node1, Distance, _)),
+        assert(edge(Node2, Node1, Distance, 0)),
+        format('Delay removed between ~w and ~w~n', [Node1, Node2])
+    ->  true
     ;   format('No edge exists between the specified nodes.~n'),
         fail
     ).
@@ -126,45 +151,24 @@ remove_edge(Node1, Node2) :-
 % Adds an edge between Node1 and Node2 with the specified Distance.
 
 add_edge(Node1, Node2, Distance) :-
-    \+ edge(Node1, Node2, _),
-    assert(edge(Node1, Node2, Distance)),
-    assert(edge(Node2, Node1, Distance)),
+    \+ edge(Node1, Node2, _, _),
+    assert(edge(Node1, Node2, Distance, 0)),
+    assert(edge(Node2, Node1, Distance, 0)),
     format('Edge added between ~w and ~w with distance ~w~n', [Node1, Node2, Distance]).
 add_edge(Node1, Node2, _) :-
-    edge(Node1, Node2, _),
+    edge(Node1, Node2, _, _),
     format('Edge already exists between ~w and ~w~n', [Node1, Node2]).
 
 % =========================
 % Reset Functions
 % =========================
 
-% reset_edges/0
-% Resets the edges to their original state.
-
-reset_edges :-
-    retractall(edge(_, _, _)),
-    % Re-define the edges in both directions
-    assert(edge(a, b, 5)), assert(edge(b, a, 5)),
-    assert(edge(a, c, 10)), assert(edge(c, a, 10)),
-    assert(edge(b, c, 2)), assert(edge(c, b, 2)),
-    assert(edge(b, d, 3)), assert(edge(d, b, 3)),
-    assert(edge(c, d, 1)), assert(edge(d, c, 1)),
-    assert(edge(c, e, 7)), assert(edge(e, c, 7)),
-    assert(edge(d, e, 2)), assert(edge(e, d, 2)),
-    assert(edge(d, f, 3)), assert(edge(f, d, 3)),
-    assert(edge(e, f, 1)), assert(edge(f, e, 1)).
-
-% reset_speeds/0
-% Resets the transportation modes to their initial speeds.
-
-% Already defined above
-
 % reset_all/0
 % Resets the edges and transportation modes to their initial states.
 
 reset_all :-
     reset_edges,
-    reset_speeds.
+    reset_modes.
 
 % =========================
 % Main Predicate
@@ -175,7 +179,7 @@ reset_all :-
 
 find_route(Start, Goal, Mode, Path, Distance, Time) :-
     % Ensure the mode is valid
-    mode_speed(Mode, _),
+    mode(Mode, _),
     % Find the shortest path
     shortest_path(Start, Goal, Path, Distance),
     % Calculate travel time
@@ -189,18 +193,16 @@ find_route(Start, Goal, Mode, Path, Distance, Time) :-
 % Display all the edges in the graph.
 
 show_graph :-
-    findall((N1, N2, D), edge(N1, N2, D), Edges),
+    findall((N1, N2, D, Delay), edge(N1, N2, D, Delay), Edges),
     format('Current graph edges:~n'),
-    forall(member((Node1, Node2, Distance), Edges),
-          format('Edge from ~w to ~w with distance ~w~n', [Node1, Node2, Distance])).
+    forall(member((Node1, Node2, Distance, Delay), Edges),
+          format('Edge from ~w to ~w with distance ~w and delay ~w~n', [Node1, Node2, Distance, Delay])).
 
 % =========================
 % Example Usage
 % =========================
 
 /*
-To use this Prolog program, you can consult it in your Prolog interpreter or from Python using PySwip.
-
 Examples:
 
 ?- find_route(a, f, car, Path, Distance, Time).
@@ -208,19 +210,17 @@ Path = [a, b, c, d, e, f],
 Distance = 13,
 Time = 0.21666666666666667.
 
-?- remove_edge(c, d).
-Obstacle introduced between c and d
+?- add_delay(c, d, 5).
+Delay of 5 added between c and d
 
 ?- find_route(a, f, car, Path, Distance, Time).
 Path = [a, b, d, e, f],
 Distance = 11,
 Time = 0.18333333333333332.
 
-?- remove_edge(c, d).
-No edge exists between the specified nodes.
+?- remove_delay(c, d).
+Delay removed between c and d
 
 ?- reset_all.
 % Resets the graph and transportation modes to initial state.
-
-Note: If no path is found due to obstacles, the query will fail.
 */
